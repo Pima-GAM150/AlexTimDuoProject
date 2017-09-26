d18d4c8034c594ab2ae96bff2474464786b7c284
Darmek <adunne@mail.pima.edu>
2017-09-26 00:03:56 -0700

    fixed tiling problem where lines were showing on floor by adding a new material. added a camera script to follow player.

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControler : MonoBehaviour {
+public class PlayerController : MonoBehaviour {
 
     public float moveSpeed;
 
