d51b1e0ef88fe08f2ef980d2929d7d2eba8491e3
LAPTOP-RR70KLG2\ezio9 <tpreble@mail.pima.edu>
2017-10-02 10:39:08 -0700

    Added larger rough dungeon map for placeholder.  Added bomb and bomb wall to dungeon scene for testing and debugging.  Added bomb and bomb wall scripts (unfinished).

---
+++ Assets/scripts/BombScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombScript : MonoBehaviour {
+
+    public Rigidbody2D Bomb;
+    public BoxCollider2D BombBoxCollider;
+
+	// Use this for initialization
+	void Start () {
+
+        BombBoxCollider.transform.localScale += new Vector3(2, 2, 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/scripts/BombWallScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombWallScript : MonoBehaviour {
+
+    public BoxCollider2D BombWall;
+    public bool HitByBomb;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D (Collision2D coll)
+    {
+        if (coll.gameObject.tag == "bomb")
+        {
+            print("A bomb is hitting the wall!");
+            HitByBomb = true;
+        }
+    }
+}
