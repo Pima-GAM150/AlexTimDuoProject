f8a67c664aefc9d9a9e84875f53e81bfc221ef50
LAPTOP-RR70KLG2\ezio9 <tpreble@mail.pima.edu>
2017-10-03 14:39:25 -0700

    Yet more changes to BombWall script. Still no luck. Collision is behaving weirdly.

--- Assets/scripts/BombWallScript.cs
+++ Assets/scripts/BombWallScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BombWallScript : MonoBehaviour {
 
     public BoxCollider2D BombWall;
-    bool HitByBomb;
+    public bool HitByBomb;
 
 	// Use this for initialization
 	void Start () {
@@ -14,17 +14,28 @@ public class BombWallScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        if (HitByBomb == true)
+        {
+            Destroy(BombWall);
+            print("Destroying the wall!");
+        }
+        else if (HitByBomb == false)
+        {
+            print("I can't destroy the wall!");
+        }
 	}
 
-    private void OnCollisionEnter2D (Collision2D coll)
+    void OnCollisionEnter2D(Collision2D coll)
     {
         if (coll.gameObject.tag.Equals("Bomb"))
         {
             print("A bomb is hitting the wall!");
             HitByBomb = true;
-            Destroy(coll.gameObject);
-            Destroy(gameObject);
+        }
+        else
+        {
+            HitByBomb = false;
         }
     }
 }
