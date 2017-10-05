1e2b7512e4bf762bb873e73235f074f6db35fc3a
LAPTOP-RR70KLG2\ezio9 <tpreble@mail.pima.edu>
2017-10-04 23:13:08 -0700

    Bombs now have a timer and destroy themselves on explosion.

--- Assets/scripts/BombScript.cs
+++ Assets/scripts/BombScript.cs
@@ -5,16 +5,36 @@ using UnityEngine;
 public class BombScript : MonoBehaviour {
 
     public BoxCollider2D BombBoxCollider;
+    public int bombTime;
 
 	// Use this for initialization
 	void Start () {
-
-        BombBoxCollider.transform.localScale += new Vector3(1.5f, 1.5f, 1.5f);
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        if (bombTime <= 120)
+        {
+            bombTime += 1;
+        }
+
+        else if (bombTime == 121)
+        {
+            BombBoxCollider.transform.localScale += new Vector3(1.5f, 1.5f, 1.5f);
+            bombTime += 1;
+        }
+
+        else if (bombTime == 122 || bombTime == 123)
+        {
+            bombTime += 1;
+        }
+
+        else if (bombTime > 123)
+        {
+            Destroy(gameObject);
+        }
+
+    }
 }
