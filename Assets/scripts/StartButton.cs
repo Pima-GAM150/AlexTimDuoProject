ed962dee58fe67fbfeca1b659d2116436a08578a
LAPTOP-RR70KLG2\ezio9 <tpreble@mail.pima.edu>
2017-10-11 21:33:33 -0700

    Added a (boring) start menu!

---
+++ Assets/scripts/StartButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour {
+
+    public void StartGameButton(string newGameLevel)
+    {
+        SceneManager.LoadScene(newGameLevel);
+        print("Swapping to main scene");
+    }
+}
