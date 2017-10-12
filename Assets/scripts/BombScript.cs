ad5e7fa65f1feafb037fa2a665d7521763a22f20
LAPTOP-RR70KLG2\ezio9 <tpreble@mail.pima.edu>
2017-10-11 23:33:36 -0700

    Pressing "E" now creates a bomb, but the bomb does not spawn near the player. This needs to be fixed.

--- Assets/scripts/BombScript.cs
+++ Assets/scripts/BombScript.cs
@@ -9,7 +9,7 @@ public class BombScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
+        //BombBoxCollider.transform.position = player character position;
 	}
 	
 	// Update is called once per frame
